b537446f5a95660505dd99084f30d2f6e21043a3
David Russell <davidrussell327@gmail.com>
2021-12-27 17:24:20 -0500

    Initial hackery toward mmsegmentation integration

--- src/main.py
+++ src/main.py
@@ -2,28 +2,29 @@
 
 # -*- encoding: utf-8 -*-
 
-import os
-
-import rospy
-from sensor_msgs.msg import Image
-from model import Mask_R_CNN
 import json
-from CMU_Mask_R_CNN.msg import predictions
-import cv2 as cv
-from vision_msgs.msg import BoundingBox2D
-from geometry_msgs.msg import Pose2D
-from cv_converter import CV_Converter
-from std_msgs.msg import Header
-import numpy as np
+import os
 import time
+
+import cv2 as cv
+import numpy as np
+import rospy
+from CMU_Mask_R_CNN.msg import predictions
+from cv_converter import CV_Converter
+from geometry_msgs.msg import Pose2D
+from model import Mask_R_CNN
+from sensor_msgs.msg import Image
+from std_msgs.msg import Header
+from vision_msgs.msg import BoundingBox2D
 
 
 class CNN_Node:
     def __init__(self):
-        path = os.path.join(os.path.dirname(__file__), '..', 'cfg', 'config.json')
+        # TODO update this
+        path = os.path.join(os.path.dirname(__file__), "..", "cfg", "config.json")
 
-        with open(path) as file:
-            self.cfg = json.load(file)
+        with open(path) as infile_h:
+            self.cfg = json.load(infile_h)
 
         self.model = Mask_R_CNN(self.cfg)
 
@@ -32,14 +33,15 @@ class CNN_Node:
         # Initialize the subscribers last or else the callback will trigger
         # when the model hasn't been created
         self.sub_rectified = rospy.Subscriber(
-            '/mapping/left/image_raw', Image, self.image_callback)
+            "/mapping/left/image_raw", Image, self.image_callback
+        )
         self.pub_predictions = rospy.Publisher(
-            '/cnn_predictions', predictions, queue_size=1)
-        self.pub_results = rospy.Publisher(
-            '/cnn_vis', Image, queue_size=1)
+            "/cnn_predictions", predictions, queue_size=1
+        )
+        self.pub_results = rospy.Publisher("/cnn_vis", Image, queue_size=1)
 
     def image_callback(self, data):
-        if not data.header.seq % self.cfg['use_image_every'] == 0:
+        if not data.header.seq % self.cfg["use_image_every"] == 0:
             return
         cv_img = self.cv_converter.msg_to_cv(data)
 
@@ -52,26 +54,34 @@ class CNN_Node:
 
         # If you wish to publish the visualized results
         visualized = self.model.visualize(cv_img, output)
-        self.pub_results.publish(self.cv_converter.cv_to_msg(cv.flip(visualized, flipCode=-1)))
+        self.pub_results.publish(
+            self.cv_converter.cv_to_msg(cv.flip(visualized, flipCode=-1))
+        )
 
         # Convert outputs from forward call to predictions message
         pub_msg = predictions()
         pub_msg.header = Header(stamp=data.header.stamp)
 
         pub_msg.scores = scores
-        pub_msg.bboxes = [BoundingBox2D(center=Pose2D(x=i[0], y=i[1]), size_x=i[2], size_y=i[3]) for i in bboxes]
-        pub_msg.masks = [self.cv_converter.cv_to_msg(masks[i], mono=True) for i in range(masks.shape[0])]
+        pub_msg.bboxes = [
+            BoundingBox2D(center=Pose2D(x=i[0], y=i[1]), size_x=i[2], size_y=i[3])
+            for i in bboxes
+        ]
+        pub_msg.masks = [
+            self.cv_converter.cv_to_msg(masks[i], mono=True)
+            for i in range(masks.shape[0])
+        ]
 
         pub_msg.source_image = self.cv_converter.cv_to_msg(cv.flip(cv_img, flipCode=-1))
 
         self.pub_predictions.publish(pub_msg)
 
 
-if __name__ == '__main__':
-    rospy.init_node('cnn', log_level=rospy.INFO)
+if __name__ == "__main__":
+    rospy.init_node("cnn", log_level=rospy.INFO)
 
     cnn_node = CNN_Node()
 
-    rospy.loginfo('started cnn node')
+    rospy.loginfo("started cnn node")
 
     rospy.spin()
--- src/model.py
+++ src/model.py
@@ -1,54 +1,28 @@
-from detectron2.data.catalog import MetadataCatalog
-from detectron2.engine import DefaultPredictor
-from detectron2.config import get_cfg
-from detectron2 import model_zoo
-from detectron2.utils.visualizer import Visualizer, ColorMode
+import init_segmentor, inference_segmentor
 
-class Mask_R_CNN:
-    def __init__(self, cfg):
-        self.model_cfg = get_cfg()
-        self.model_cfg.merge_from_file(
-            model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
-        )
-        self.model_cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = cfg['score_threshold']
-        self.model_cfg.MODEL.WEIGHTS = cfg['model_path']
-        self.model_cfg.MODEL.ROI_HEADS.NUM_CLASSES = 2  # background, trunk
-        self.model_cfg.MODEL.ROI_HEADS.NMS_THRESH_TEST = 0.1
-        self.model_cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = (128)
-        self.model_cfg.MODEL.DEVICE = 'cuda:1'
 
-        self.model = DefaultPredictor(self.model_cfg)
+class Segmentor:
+    def __init__(self, config_path, checkpoint_path, device):
+
+        # build the model from a config file and a checkpoint file
+        self.model = init_segmentor(config_path, checkpoint_path, device=device)
 
     def forward(self, image):
-        '''
+        """
         Forward Prop for the Mask-R-CNN model
 
         Parameters:
             image (cv.Mat/np.ndarray): the input image
 
         Returns:
-            np.ndarray: confidence scores for each prediction
-            np.ndarray: (N X 4) array of bounding box corners
-            np.ndarray: (N X H X W) masks for each prediction,
-                where the channel 0 index corresponds to the prediction index
-                and the other channels are boolean values representing whether
-                that pixel is inside that prediction's mask
-            dict: output object from detectron2 predictor
-        '''
-        outputs = self.model(image)
+            TODO
+        """
 
-        scores = outputs['instances'].scores.to('cpu').numpy()
-
-        bboxes = outputs['instances'].pred_boxes.tensor.to('cpu').numpy()
-        masks = outputs['instances'].pred_masks.to('cpu').numpy()
-
-        # Convert xyxy corner format to xywh center format
-        bboxes_center = [[(i[0] + i[2]) / 2, (i[1] + i[3]) / 2, abs(i[2] - i[0]), abs(i[3] - i[1])] for i in bboxes]
-
-        return scores, bboxes_center, masks, outputs
+        result = inference_segmentor(self.model, image)
+        return result
 
     def visualize(self, input_image, output):
-        '''
+        """
         Visualize the results of the model
 
         Parameters:
@@ -57,13 +31,5 @@ class Mask_R_CNN:
 
         Returns:
             np.ndarray: visualized results
-        '''
-
-        v = Visualizer(input_image[:, :, ::-1],
-                       metadata=MetadataCatalog.get('empty'), 
-                       scale=0.8,
-                       instance_mode=ColorMode.IMAGE_BW  # remove the colors of unsegmented pixels
-        )
-        v = v.draw_instance_predictions(output['instances'].to('cpu'))
-        
-        return v.get_image()[:, :, ::-1]
+        """
+        raise NotImplementedError
